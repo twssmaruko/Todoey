f8dd49e9cbecdba6fb01f4b4e464b61995615259
Marco Tan <tanmarcogabriel.com>
2019-08-30 14:54:28 +0800

    Convert to MVC Design Pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Marie Gloria Tan on 30/08/2019.
+//  Copyright © 2019 Marco Tan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done: Bool = false
+    
+    
+}
